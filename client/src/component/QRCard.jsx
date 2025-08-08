4771ccaf8dc690d87e5a0b2e8b667b6872bf986d
nguyenkhanhduyit <nguyenkhanhduy.dev@gmail.com>
2025-08-08 19:27:24 +0700

    chỉnh darkmode cho qr

--- client/src/component/QRCard.jsx
+++ client/src/component/QRCard.jsx
@@ -5,10 +5,12 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import CloseIcon from '@mui/icons-material/Close';
 
 const QRCard = ({ show, theme, onClose }) => {
+
   const { currentAccount } = useContext(TransactionContext);
   const [showQR, setShowQR] = useState(show);
   const canvasRef = useRef(null);
   const modalRef = useRef(null); 
+  const [isCopy,setIsCopy] = useState(false)
 
   useEffect(() => {
     setShowQR(show);
@@ -25,51 +27,44 @@ const QRCard = ({ show, theme, onClose }) => {
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (modalRef.current && !modalRef.current.contains(event.target)) {
-        setShowQR(false);
-        if (onClose) onClose(); 
+        setShowQR(false)
+        setIsCopy(false)
+        if (onClose) onClose();
       }
     };
 
-    if (showQR) {
+    if (showQR)
       document.addEventListener('mousedown', handleClickOutside);
-    }
 
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [showQR, onClose]);
-
-  if (!showQR || !currentAccount) return null;
+      setIsCopy(false)
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [showQR, onClose])
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#eae6e800]">
       <div
         ref={modalRef}
-        className={`p-3 max-h-[370px] max-w-[250px] min-h-[370px] min-w-[250px] rounded-lg shadow-xl
-          ${theme === 'dark-mode' ? 'bg-[#272a34] text-white' : 'bg-blue-100 text-gray-900'}`}
+        className={`p-3 max-h-[370px] max-w-[250px] min-h-[310px] min-w-[250px] rounded-lg shadow-xl
+          ${theme === 'dark-mode' ? 'qr-dark-mode-background text-white' : 'bg-blue-100 text-gray-900'}`}
       >
         <div className="flex-col justify-center items-center ">
           <div className="flex justify-end my-2">
-            <button onClick={() => {
-              setShowQR(false);
-              if (onClose) onClose();
-            }}>
-              <CloseIcon />
-            </button>
           </div>
           <div className="flex justify-center mb-4">
             <canvas ref={canvasRef} />
           </div>
           <p className="text-wrap break-words font-bold text-sm">{currentAccount}
              <span className='p-2'>
-                <button  onClick={() => navigator.clipboard.writeText(currentAccount)}>
+                <button  onClick={() => {navigator.clipboard.writeText(currentAccount);setIsCopy(true)}}>
                         <ContentCopyIcon />
                 </button>
-          </span>
+                {
+                  isCopy && <span>Copied</span> 
+                }
+            </span>
           </p>
-          
-           
-         
         </div>
       </div>
     </div>
