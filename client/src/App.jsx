8a115c0a139fd09c93d3759a0c30243f025ecb65
nguyenkhanhduyit <nguyenkhanhduy.dev@gmail.com>
2025-08-12 23:35:13 +0700

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,46 +7,50 @@ import Faucet from './component/Faucet';
 
 function App() {
 
-    const [theme, setTheme] = useState(
-      () => {
-        const temp = localStorage.getItem("theme");
-        if (temp === "light-mode" || temp === "dark-mode") 
-            return temp;
-        return "dark-mode"; 
-        }
-      )
+const [theme, setTheme] = useState(
+  () => {
+    const temp = localStorage.getItem("theme");
+    if (temp === "light-mode" || temp === "dark-mode") 
+      return temp;
+    return "dark-mode"; 
+    }
+  )
 
-  const saveTheme = (n) => {
-    setTheme(n)
-    localStorage.setItem("theme",n)
-  }
+const saveTheme = (n) => {
+  setTheme(n)
+  localStorage.setItem("theme",n)
+}
 
 useEffect(()=>{
   const checkTheme = localStorage.getItem('theme')
   if(!checkTheme) localStorage.setItem("theme","dark-mode")
 },[])
 
-  return (
-    <BrowserRouter>
-      <div className={`${theme === "dark-mode" ? "dark-mode" : "light-mode"} flex flex-col`}>
-        <Navbar theme={theme} setTheme={saveTheme} />
-        
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={
-              <>
-              <Welcome theme={theme}/>
-              <Services theme={theme}/>
-              </>
-            } />
-            <Route path='/faucet' element={<Faucet theme={theme}/>}/>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer theme={theme}/>
-      </div>
-    </BrowserRouter>
-  );
+useEffect(() => {
+  document.body.classList.remove("dark-mode", "light-mode");
+  document.body.classList.add(theme);
+}, [theme]);
+
+return (
+<BrowserRouter>
+  <div className={`flex flex-col`}>
+    <Navbar theme={theme} setTheme={saveTheme} />
+    <main className="flex-grow">
+      <Routes>
+        <Route path="/" element={
+          <>
+          <Welcome theme={theme}/>
+          <Services theme={theme}/>
+          </>
+        } />
+        <Route path='/faucet' element={<Faucet theme={theme}/>}/>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </main>
+    <Footer theme={theme}/>
+  </div>
+</BrowserRouter>
+);
 }
 
 export default App;
