e81102ce319f997fd7dcaa1be80ef0da0f063daf
nguyenkhanhduyit <nguyenkhanhduy.dev@gmail.com>
2025-09-03 20:46:55 +0700

    ..

--- services/server.js
+++ services/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express()
 app.use(cors({
-  origin: true,
+  origin: 'https://dit-web3.vercel.app/',
   credentials: true           
 }))
 app.use(bodyParser.json())
